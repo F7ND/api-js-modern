22f3bfdf9f035e5a3aa9152703aa1bd2c2dc4481
f7nd <arafandiarafan@gmail.com>
2026-06-25 23:50:26 +0700

    yeee

--- api/brainly.js
+++ api/brainly.js
@@ -1,5 +1,4 @@
-// api/brainly.js
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -79,7 +78,7 @@ module.exports = async (req, res) => {
       error: error.message,
     });
   }
-};
+}
 
 function parseBrainlyResponse(jsonResponse) {
   const results = jsonResponse?.data?.results || [];
